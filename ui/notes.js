c2b33084f9da30259f830bfa246778865646dfcb
Andrew Rivett <veggiefrog@gmail.com>
2015-11-21 14:01:36 -0500

    Added citations

--- ui/notes.js
+++ ui/notes.js
@@ -24,21 +24,44 @@ function ciniki_materiamedica_notes() {
 			'_content':{'label':'', 'type':'simpleform', 'fields':{
 				'content':{'label':'', 'type':'textarea', 'size':'medium', 'hidelabel':'yes'},
 			}},
+            'citations':{'label':'References', 'type':'simplegrid', 'num_cols':1,
+                'addTxt':'Add Reference',
+                'addFn':'M.ciniki_materiamedica_notes.citationEdit(0);',
+                },
 			'_save':{'label':'', 'buttons':{
 				'save':{'label':'Save', 'fn':'M.ciniki_materiamedica_notes.noteSave();'},
 				'delete':{'label':'Delete', 'visible':'no', 'fn':'M.ciniki_materiamedica_notes.noteDelete();'},
 			}},
 		};
+        this.edit.sectionData = function(s) { return this.data[s]; }
 		this.edit.fieldValue = function(s, i, d) { 
 			if( this.data[i] != null ) {
 				return this.data[i]; 
 			} 
 			return ''; 
 		};
+        this.edit.cellValue = function(s, i, j, d) {
+            return d.citation_text;
+        };
+        this.edit.rowFn = function(s, i, d) {
+            return 'M.ciniki_materiamedica_notes.citationEdit(' + d.id + ');';
+        };
 		this.edit.fieldHistoryArgs = function(s, i) {
 			return {'method':'ciniki.materiamedica.noteHistory', 'args':{'business_id':M.curBusinessID, 
 				'note_id':this.note_id, 'field':i}};
 		};
+        this.edit.refreshCitations = function() {
+            M.api.getJSONCb('ciniki.materiamedica.noteGet', {'business_id':M.curBusinessID, 'note_id':this.note_id, 'citations':'yes'}, function(rsp) {
+                if( rsp.stat != 'ok' ) {
+                    M.api.err(rsp);
+                    return false;
+                }
+                var p = M.ciniki_materiamedica_notes.edit;
+                p.data.citations = rsp.note.citations;
+                p.refreshSection('citations');
+                p.show();
+            });
+        };
 		this.edit.addButton('save', 'Save', 'M.ciniki_materiamedica_notes.noteSave();');
 		this.edit.addClose('Cancel');
 	};
@@ -55,6 +78,8 @@ function ciniki_materiamedica_notes() {
 			alert('App Error');
 			return false;
 		}
+
+        this.edit.sections.citations.visible = (M.curBusiness.modules['ciniki.citations'] != null?'yes':'no');
 
         this.noteEdit(cb, args.note_key, (args.note_id==null?0:args.note_id));
 	}
@@ -73,6 +98,24 @@ function ciniki_materiamedica_notes() {
             p.show(cb);
         });
 	};
+
+    this.citationEdit = function(cid) {
+        if( this.edit.note_id == 0 ) {
+			var c = this.edit.serializeForm('yes');
+			M.api.postJSONCb('ciniki.materiamedica.noteAdd', {'business_id':M.curBusinessID, 'note_key':this.edit.note_key}, c,
+                function(rsp) {
+                    if( rsp.stat != 'ok' ) {
+                        M.api.err(rsp);
+                        return false;
+                    } 
+                    M.startApp('ciniki.citations.edit',null,'M.ciniki_materiamedica_notes.edit.refreshCitations();','mc',
+                        {'object':'ciniki.materiamedica.note','object_id':this.edit.note_id,'citation_id':cid});
+                });
+        } else {
+            M.startApp('ciniki.citations.edit',null,'M.ciniki_materiamedica_notes.edit.refreshCitations();','mc',
+                {'object':'ciniki.materiamedica.note','object_id':this.edit.note_id,'citation_id':cid});
+        }
+    };
 
 	this.noteSave = function() {
 		if( this.edit.note_id > 0 ) {
